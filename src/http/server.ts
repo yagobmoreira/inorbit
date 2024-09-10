15e7782f0f835801262362e524e34593f9503213
Yago Moreira <yagomoreira@gmail.com>
2024-09-10 12:00:21 -0300

    feat: add create goal route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { createGoal } from '../services/create-goal'
+import z from 'zod'
 
 const app = fastify()
+
+app.post('/goals', async request => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
