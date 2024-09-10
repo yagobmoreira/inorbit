20b806b62d636c952c5639ff0f8d183cceee5301
Yago Moreira <yagomoreira@gmail.com>
2024-09-10 13:04:42 -0300

    feat: add get week pending goals route

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,11 +6,18 @@ import {
 } from 'fastify-type-provider-zod'
 import { createGoal } from '../services/create-goal'
 import z from 'zod'
+import { getWeekPendingGoals } from '../services/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
